94c12be10913fef74a0608c634896146f22a7ef0
Musa <puzzlemusa@gmail.com>
2022-03-23 00:14:01 +0100

    Add LC RotateArray

---
+++ ProblemSolving/LeetCode/RotateArray.cpp
@@ -0,0 +1,83 @@
+/*
+ID: puzzlemusa
+PROG:
+LANG: C++
+*/
+
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <functional>
+#include <iomanip>
+
+using namespace std;
+
+#define frn(i, n) for(i=0;i<n;i++)
+#define frN(i, n) for(i=1;i<=n;i++)
+#define frr(i, n) for(i=n-1;i>=0;i--)
+#define frv(i, a, n) for(i=a;i<n;i++)
+#define frl(i, v) for(;i!=v.end();++i)
+#define li(v) v.begin(), v.end()
+#define sz(v) ((int)v.size())
+#define emt(v) v.empty()
+#define SET(v, a) memset(v,a,sizeof(v))
+#define pi (2*acos(0))
+
+const int maxn = 2147483647;
+vector<int> v;
+
+
+class Solution {
+public:
+    typedef long long Long;
+
+    void rotate(vector<int>& nums, int k) {
+        //get TLE
+        k = k % nums.size();
+
+        int j = 0, x;
+        for(int i = nums.size()-k; i < nums.size(); i++){
+            x = nums[i];
+            nums.erase(nums.begin() + i);
+            nums.insert(nums.begin() + j, x);
+            j++;
+        }
+    }
+
+    void rotate1(vector<int>& nums, int k) {
+        k = k % nums.size();
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(0);
+#ifdef localhost
+    freopen("E://input.txt", "r", stdin);
+    //freopen("E://output.txt","w", stdout);
+#endif
+    typedef long long Long;
+    Solution sol;
+    v = {-7,-3,-2,-1,3,11};
+    sol.rotate1(v, 2);
+
+    //cin >> n;
+    return 0;
+}
