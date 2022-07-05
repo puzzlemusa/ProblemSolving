9337545f1d458d86ac871f037677f4bf6f575b45
Musa <a.musa@sevensenders.com>
2022-07-05 10:56:17 +0200

    Add RemoveNthNodeFromEndOfList

---
+++ ProblemSolving/src/LeetCode/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,56 @@
+package LeetCode;
+
+
+public class RemoveNthNodeFromEndOfList {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode one = new ListNode(1);
+        ListNode two = new ListNode(2);
+        ListNode three = new ListNode(3);
+        ListNode four = new ListNode(4);
+        ListNode five = new ListNode(5);
+
+        four.next = five;
+        three.next = four;
+        two.next = three;
+        one.next = two;
+
+        ListNode res = removeNthFromEnd(one, 4);
+    }
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode start = new ListNode(0);
+        ListNode slow = start;
+        ListNode fast = start;
+        slow.next = head;
+        for (int i = 1; i <= n+1; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return start.next;
+    }
+}
