f69be9ee6a7534d3c4cb4bd675e3db8493c35a59
Musa <a.musa@sevensenders.com>
2022-08-10 14:31:57 +0200

    Add IntersectionOfTwoLinkedLists

---
+++ ProblemSolving/src/LeetCode/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,56 @@
+package LeetCode;
+
+
+public class IntersectionOfTwoLinkedLists {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode one = new ListNode(1);
+        ListNode two = new ListNode(2);
+        ListNode three = new ListNode(3);
+        ListNode four = new ListNode(4);
+        ListNode five = new ListNode(5);
+        ListNode six = new ListNode(6);
+        ListNode seven = new ListNode(7);
+        ListNode eight = new ListNode(8);
+
+        five.next = six;
+        four.next = five;
+        three.next = four;
+
+        two.next = three;
+        one.next = two;
+
+        //seven.next = three;
+
+        System.out.println(getIntersectionNode(one, seven));
+    }
+
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode pointerA = headA;
+        ListNode pointerB = headB;
+
+        while (pointerA != pointerB){
+            pointerA = pointerA == null ? headB : pointerA.next;
+            pointerB = pointerB == null ? headA : pointerB.next;
+        }
+
+        return pointerA;
+    }
+}
