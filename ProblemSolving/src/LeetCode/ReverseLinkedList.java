408786224254c6c956d07b289d567b29912d0def
Musa <a.musa@sevensenders.com>
2022-11-07 15:25:16 +0100

    Add ReverseLinkedList

--- ProblemSolving/src/LeetCode/ReverseLinkedList.java
+++ ProblemSolving/src/LeetCode/ReverseLinkedList.java
@@ -1,18 +1,26 @@
 package LeetCode;
 
 
-
 public class ReverseLinkedList {
 
     public static class ListNode {
         int val;
         ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ListNode one = new ListNode(1);
         ListNode two = new ListNode(2);
         ListNode three = new ListNode(3);
@@ -22,13 +30,16 @@ public class ReverseLinkedList {
         two.next = three;
         one.next = two;
 
-        ListNode result = reverseList(one);
+        //ListNode result = reverseList(one);
+        ListNode result1 = reverseListRecursive(one);
+
+        System.out.println("asdf");
     }
 
     public static ListNode reverseList(ListNode head) {
         ListNode result = null;
         ListNode next = null;
-        while(head!=null){
+        while (head != null) {
             next = head.next;
             head.next = result;
             result = head;
@@ -36,5 +47,17 @@ public class ReverseLinkedList {
         }
 
         return result;
+    }
+
+    public static ListNode reverseListRecursive(ListNode head) {
+        return reverse(head, null);
+    }
+
+    private static ListNode reverse(ListNode head, ListNode newNode) {
+        if (head == null)
+            return newNode;
+        ListNode next = head.next;
+        head.next = newNode;
+        return reverse(next, head);
     }
 }
