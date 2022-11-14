290b2b39936eddf41cc066ba04194f5a29220479
Musa <a.musa@sevensenders.com>
2022-11-14 13:02:26 +0100

    Add BinarySearch

---
+++ ProblemSolving/src/LeetCode/BinarySearch.java
@@ -0,0 +1,30 @@
+package LeetCode;
+
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        System.out.println(search(new int[]{-1, 0, 3, 5, 9, 12}, 9));
+        System.out.println(search(new int[]{-1, 0, 3, 5, 9, 12}, 2));
+        System.out.println(search(new int[]{-1, 0, 3, 5, 9, 12}, -2));
+        System.out.println(search(new int[]{-1, 0, 3, 5, 9, 12}, 14));
+        System.out.println(search(new int[]{-1, 0, 3, 5, 9, 12}, -1));
+        System.out.println(search(new int[]{-1, 0, 3, 5, 9, 12}, 12));
+    }
+
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (target > nums[mid])
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return -1;
+    }
+}
