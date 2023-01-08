286770655bd3900d1a4b9d8abc99a4ca1d74d9d7
Musa <a.musa@sevensenders.com>
2023-01-08 18:06:02 +0100

    Add SearchInRotatedSortedArray

---
+++ ProblemSolving/src/LeetCode/SearchInRotatedSortedArray.java
@@ -0,0 +1,46 @@
+package LeetCode;
+
+
+public class SearchInRotatedSortedArray {
+
+    public static void main(String[] args) {
+        System.out.println(search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
+        System.out.println(search(new int[]{5, 6, 7, 0, 1, 2, 4}, 6));
+        System.out.println(search(new int[]{6, 7, 0, 1, 2, 4, 5}, 5));
+        System.out.println(search(new int[]{6, 7, 0, 1, 2, 4, 5}, 6));
+        System.out.println(search(new int[]{0, 1, 2, 4, 5}, 0));
+        System.out.println(search(new int[]{3, 1}, 1));
+    }
+
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[right] == target) {
+                return right;
+            }
+
+            if (nums[left] < nums[mid]) {
+                if (target < nums[mid] && target >= nums[left]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return nums[left] == target ? left : -1;
+    }
+}
