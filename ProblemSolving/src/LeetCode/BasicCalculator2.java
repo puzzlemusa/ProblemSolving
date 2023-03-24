04fa4b70930bb8b6de25abe3a9f7ef16ed9d3d72
Musa <a.musa@sevensenders.com>
2023-03-24 22:39:21 +0100

    Add CourseSchedule2

---
+++ ProblemSolving/src/LeetCode/BasicCalculator2.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+
+import java.util.*;
+
+public class BasicCalculator2 {
+
+    public static void main(String[] args) {
+        System.out.println(calculate("3+2*2"));
+        System.out.println(calculate(" 3/2 "));
+        System.out.println(calculate(" 3+5 / 2 "));
+    }
+
+    public static int calculate(String s) {
+
+    }
+}
