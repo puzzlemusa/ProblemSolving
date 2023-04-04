f1d81df69c140b66e99ec10ccbce7ca8bfcac2ce
Musa <a.musa@sevensenders.com>
2023-04-04 22:45:17 +0200

    Add SearchA2DMatrixII

---
+++ ProblemSolving/src/LeetCode/SearchA2DMatrixII.java
@@ -0,0 +1,39 @@
+package LeetCode;
+
+
+public class SearchA2DMatrixII {
+
+    public static void main(String[] args) {
+        System.out.println(searchMatrix(new int[][]{
+                        {1, 4, 7, 11, 15},
+                        {2, 5, 8, 12, 19},
+                        {3, 6, 9, 16, 22},
+                        {10, 13, 14, 17, 24},
+                        {18, 21, 23, 26, 30}},
+                4));
+        System.out.println(searchMatrix(new int[][]{
+                        {1, 4, 7, 11, 15},
+                        {2, 5, 8, 12, 19},
+                        {3, 6, 9, 16, 22},
+                        {10, 13, 14, 17, 24},
+                        {18, 21, 23, 26, 30}},
+                20));
+    }
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix[0].length - 1;
+
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (target > matrix[row][col]) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+
+        return false;
+    }
+}
