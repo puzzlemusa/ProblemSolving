b7c0a350803407447d08f36279565fe382b18a5a
Musa <a.musa@sevensenders.com>
2023-12-18 20:17:53 +0600

    Add CarFleet

---
+++ ProblemSolving/src/LeetCode/CarFleet.java
@@ -0,0 +1,33 @@
+package LeetCode;
+
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class CarFleet {
+
+    public static void main(String[] args) {
+        System.out.println(carFleet(12, new int[]{10, 8, 0, 5, 3}, new int[]{2, 4, 1, 1, 3}));
+        System.out.println(carFleet(10, new int[]{3}, new int[]{3}));
+        System.out.println(carFleet(100, new int[]{0, 2, 4}, new int[]{4, 2, 1}));
+    }
+
+    public static int carFleet(int target, int[] position, int[] speed) {
+        Map<Integer, Double> mp = new TreeMap<>(Collections.reverseOrder());
+
+        for (int i = 0; i < position.length; i++) {
+            mp.put(position[i], (double) (target - position[i]) / speed[i]);
+        }
+
+        int fleet = 0;
+        double curr = 0;
+        for (double time : mp.values()) {
+            if (time > curr) {
+                curr = time;
+                fleet++;
+            }
+        }
+        return fleet;
+    }
+}
---
+++ ProblemSolving/src/LeetCode/DailyTemperatures.java
@@ -0,0 +1,28 @@
+package LeetCode;
+
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class DailyTemperatures {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(dailyTemperatures(new int[]{73, 74, 75, 71, 69, 72, 76, 73})));
+        System.out.println(Arrays.toString(dailyTemperatures(new int[]{30, 40, 50, 60})));
+        System.out.println(Arrays.toString(dailyTemperatures(new int[]{30, 60, 90})));
+    }
+
+    public static int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stk = new Stack<>();
+        int[] res = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stk.isEmpty() && temperatures[i] > temperatures[stk.peek()]) {
+                int idx = stk.pop();
+                res[idx] = i - idx;
+            }
+            stk.push(i);
+        }
+
+        return res;
+    }
+}
