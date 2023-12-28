5a69ab246f4fe52fd890abb4b9fed2bc142edfe4
Musa <a.musa@sevensenders.com>
2023-12-28 15:43:07 +0600

    Add CourseSchedule

--- ProblemSolving/src/LeetCode/CourseSchedule.java
+++ ProblemSolving/src/LeetCode/CourseSchedule.java
@@ -1,10 +1,7 @@
 package LeetCode;
 
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class CourseSchedule {
 
@@ -12,8 +9,11 @@ public class CourseSchedule {
         System.out.println(canFinish(2, new int[][]{{1, 0}}));
         System.out.println(canFinish(2, new int[][]{{0, 1}}));
         System.out.println(canFinish(2, new int[][]{{1, 0}, {0, 1}}));
+        System.out.println(canFinish1(2, new int[][]{{1, 0}, {0, 1}}));
+        System.out.println(canFinish1(2, new int[][]{{0, 1}, {1, 2}, {2, 3}}));
     }
 
+    // Time complexity: O(V + E), space complexity: O(V + E)
     public static boolean canFinish(int numCourses, int[][] prerequisites) {
         List<Integer>[] adjacentList = new ArrayList[numCourses];
         int[] inDegrees = new int[numCourses];
@@ -45,5 +45,54 @@ public class CourseSchedule {
         }
 
         return count == numCourses;
+    }
+
+    public static boolean canFinish1(int numCourses, int[][] prerequisites) {
+        Map<Integer, Integer> inDegree = new HashMap<>();
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        for (int[] pre : prerequisites) {
+            if (!inDegree.containsKey(pre[0])) {
+                inDegree.put(pre[0], 1);
+            } else {
+                inDegree.put(pre[0], inDegree.get(pre[0]) + 1);
+            }
+            if (!inDegree.containsKey(pre[1])) {
+                inDegree.put(pre[1], 0);
+            }
+            if (!adj.containsKey(pre[1])) {
+                List<Integer> ad = new ArrayList<>();
+                ad.add(pre[0]);
+                adj.put(pre[1], ad);
+            } else {
+                List<Integer> ad = adj.get(pre[1]);
+                ad.add(pre[0]);
+                adj.put(pre[1], ad);
+            }
+            if (!adj.containsKey(pre[0])) {
+                adj.put(pre[0], new ArrayList<>());
+            }
+        }
+
+        List<Integer> zeroDegree = new ArrayList<>();
+        for (Map.Entry entry : inDegree.entrySet()) {
+            if ((int) entry.getValue() == 0) {
+                zeroDegree.add((int) entry.getKey());
+            }
+        }
+
+        List<Integer> sorted = new ArrayList<>();
+        while (!zeroDegree.isEmpty()) {
+            int node = zeroDegree.get(zeroDegree.size() - 1);
+            sorted.add(node);
+            zeroDegree.remove(zeroDegree.size() - 1);
+            for (int ad : adj.get(node)) {
+                inDegree.put(ad, inDegree.get(ad) - 1);
+                if (inDegree.get(ad) == 0) {
+                    zeroDegree.add(ad);
+                }
+            }
+        }
+
+        return sorted.size() == inDegree.size();
     }
 }
