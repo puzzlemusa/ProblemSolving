89fea9ad80ef7f7b133dab5538985bbfa88ceb03
Musa <a.musa@sevensenders.com>
2024-01-05 19:38:23 +0600

    Add DecodeWays

--- ProblemSolving/src/LeetCode/DecodeWays.java
+++ ProblemSolving/src/LeetCode/DecodeWays.java
@@ -4,37 +4,61 @@ package LeetCode;
 public class DecodeWays {
 
     public static void main(String[] args) {
-//        System.out.println(numDecodings("12"));
-//        System.out.println(numDecodings("226"));
-//        System.out.println(numDecodings("06"));
-//        System.out.println(numDecodings("30"));
-//        System.out.println(numDecodings("31"));
-//        System.out.println(numDecodings("031111"));
-//        System.out.println(numDecodings("1"));
+        System.out.println(numDecodings1("11106"));
+        System.out.println(numDecodings1("12"));
+        System.out.println(numDecodings1("226"));
+        System.out.println(numDecodings1("06"));
+        System.out.println(numDecodings1("27"));
+        System.out.println(numDecodings("30"));
+        System.out.println(numDecodings("31"));
+        System.out.println(numDecodings("031111"));
+        System.out.println(numDecodings("1"));
         System.out.println(numDecodings("102331"));
         System.out.println(numDecodings("1111"));
+        System.out.println(numDecodings("10"));
     }
 
+    // Time Complexity: O(n), space complexity: O(n)
     public static int numDecodings(String s) {
-        if (s.length() == 0) {
-            return 0;
-        }
         int n = s.length();
         int[] mem = new int[n + 1];
         mem[0] = 1;
         mem[1] = s.charAt(0) == '0' ? 0 : 1;
 
         for (int i = 2; i <= s.length(); i++) {
-            int curr = s.charAt(i-1) - '0';
+            int curr = s.charAt(i - 1) - '0';
             int last2 = curr + ((s.charAt(i - 2) - '0') * 10);
 
-            if (curr >= 1 && curr <= 9) {
-                mem[i] += mem[i - 1];
+            if (curr != 0) {
+                mem[i] = mem[i - 1];
             }
-            if (last2 >= 10 && curr <= 26) {
+
+            if (last2 >= 10 && last2 <= 26) {
                 mem[i] += mem[i - 2];
             }
         }
         return mem[s.length()];
+    }
+
+    // Time Complexity: O(n), space complexity: O(1)
+    public static int numDecodings1(String s) {
+        int dp1 = 1;
+        int dp2 = s.charAt(0) == '0' ? 0 : 1;
+
+        for (int i = 2; i <= s.length(); i++) {
+            int curr = s.charAt(i - 1) - '0';
+            int last2 = curr + ((s.charAt(i - 2) - '0') * 10);
+            int temp = 0;
+            if (curr != 0) {
+                temp = dp2;
+            }
+
+            if (last2 >= 10 && last2 <= 26) {
+                temp += dp1;
+            }
+            dp1 = dp2;
+            dp2 = temp;
+        }
+        return dp2;
     }
 }
---
+++ ProblemSolving/src/LeetCode/NumberOfPaths.java
@@ -0,0 +1,103 @@
+package LeetCode;
+
+
+public class NumberOfPaths {
+
+    public static void main(String[] args) {
+        System.out.println(numOfPathsToDest(1));
+        System.out.println(numOfPathsToDest(2));
+        System.out.println(numOfPathsToDest(3));
+        System.out.println(numOfPathsToDest3(4));
+        System.out.println(numOfPathsToDest3(5));
+        System.out.println(numOfPathsToDest3(6));
+    }
+
+    // Time complexity: O(2^(n^2)), space complexity: O(n^2)
+    static int numOfPathsToDest(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return dfs(1, 0, n);
+    }
+
+    static int dfs(int i, int j, int n) {
+        if (i == n - 1 && j == n - 1) {
+            return 1;
+        }
+        if (i >= n || j >= n || j > i) {
+            return 0;
+        }
+
+        return dfs(i, j + 1, n) + dfs(i + 1, j, n);
+    }
+
+    // Time complexity: O(n^2), space complexity: O(n^2)
+    static int numOfPathsToDest1(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[][] dp = new int[n][n];
+        int r = dfs1(1, 0, n, dp);
+        return dfs1(1, 0, n, dp);
+    }
+
+    static int dfs1(int i, int j, int n, int[][] dp) {
+        if (i == n - 1 && j == n - 1) {
+            return 1;
+        }
+        if (i >= n || j >= n || j > i) {
+            return 0;
+        }
+
+        if (dp[i][j] != 0) {
+            return dp[i][j];
+        }
+
+        return dp[i][j] = dfs1(i, j + 1, n, dp) + dfs1(i + 1, j, n, dp);
+    }
+
+    // Time complexity: O(n^2), space complexity: O(n^2)
+    static int numOfPathsToDest2(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (i == j) {
+                    dp[i][j] = dp[i][j - 1];
+                } else {
+                    dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n - 1][n - 1];
+    }
+
+    // Time complexity: O(n^2), space complexity: O(n)
+    static int numOfPathsToDest3(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[] last = new int[n];
+        int[] curr = new int[n];
+        for (int i = 1; i < n; i++) {
+            curr[0] = 1;
+            for (int j = 1; j <= i; j++) {
+                if (i == j) {
+                    curr[j] = curr[j - 1];
+                    last[j] = curr[j];
+                } else {
+                    curr[j] = curr[j - 1] + last[j];
+                    last[j] = curr[j];
+                }
+            }
+        }
+
+        return curr[n - 1];
+    }
+}
